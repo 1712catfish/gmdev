fe14a567428a5b3dd96039df0b9e6a9ead52e053
Minh Lê Hoàng <12A5.18.hoangminh@luongthevinhhcm.onmicrosoft.com>
2021-06-17 15:22:33 +0700

    ok mine

---
+++ platformer/TextureProgress.gd
@@ -0,0 +1,13 @@
+extends TextureProgress
+
+onready var health_bar = $TextureProgress
+
+func _on_health_updated(health):
+	health_bar.value = health
+
+func _on_max_health_updated(max_health):
+	health_bar.max_value = max_health
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
---
+++ platformer/enemy(kinematic2d).gd
@@ -0,0 +1,70 @@
+extends KinematicBody2D
+
+#only for moving:
+var POSITION = 1
+var movement = Vector2()
+const SPEED = 50
+const GRAVITY = 2700
+const  FLOOR = Vector2(0,-1)
+
+
+
+export ( float ) var max_health = 100
+onready var health = max_health 
+var first_time = true
+
+func killed():
+	$Sprite.visible = false
+	$TextureProgress.visible = false
+	$CollisionShape2D.disabled = true
+	set_physics_process(false)
+
+	
+func _set_health(value):
+	var prev_health = health
+	health = clamp(value, 0, max_health)
+	$TextureProgress.value = health
+	if health == 0:
+		killed()
+	if health != prev_health:
+		pass
+	return health
+func attacked(amount):
+	health = _set_health(health - amount)
+func _ready():
+	if first_time:
+		$TextureProgress.max_value = max_health
+		$TextureProgress.value = max_health
+		first_time = false
+	pass
+
+
+
+
+export var attack_damage = 1
+func attack():
+	var collider
+	
+	collider = $RayCast2D3.get_collider()
+	if collider:
+		var i = $RayCast2D3.get_collider_shape()
+		var body = collider.shape_owner_get_owner(i)
+		body.get_parent().attacked(attack_damage)
+		print(body.get_parent().health)
+
+
+
+func _physics_process(delta):
+	#var space_rid = get_world_2d().space
+	#var space_state = Physics2DServer.space_get_direct_state(space_rid)
+	movement.x = SPEED * POSITION
+	movement.y += GRAVITY * delta
+	movement = move_and_slide(movement,FLOOR)
+	if is_on_wall():
+		POSITION *= -1 
+		$RayCast2D.position.x *= -1
+	if $RayCast2D.is_colliding() == false: 
+		POSITION *= -1
+		$RayCast2D.position.x *= -1
+	attack()
+
